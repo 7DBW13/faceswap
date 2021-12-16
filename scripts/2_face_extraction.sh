9ee37071c0172c5472f7a917123de5407aea9813
7DBW13 <dengbw@seas.upenn.edu>
2021-12-16 13:49:28 -0500

    modify scripts

--- scripts/2_face_extraction.sh
+++ scripts/2_face_extraction.sh
@@ -8,7 +8,7 @@ $DFD_PYTHON "preprocessor.py" \
     -f "../$DFD_WORKSPACE/data_source/face_img"\
     -c "../$DFD_WORKSPACE/data_source/crop_img"\
     -t "../$DFD_WORKSPACE/data_source/trans_matrix"\
-    -r "1024"
+    -r "128"
 
 $DFD_PYTHON "preprocessor.py" \
     -i "../$DFD_WORKSPACE/data_target/origin"\
@@ -16,20 +16,4 @@ $DFD_PYTHON "preprocessor.py" \
     -f "../$DFD_WORKSPACE/data_target/face_img"\
     -c "../$DFD_WORKSPACE/data_target/crop_img"\
     -t "../$DFD_WORKSPACE/data_target/trans_matrix"\
-    -r "1024"
-
-$DFD_PYTHON "preprocessor.py" \
-    -i "../$DFD_WORKSPACE/data_dbw/origin"\
-    -m "../$DFD_WORKSPACE/data_dbw/face_mask"\
-    -f "../$DFD_WORKSPACE/data_dbw/face_img"\
-    -c "../$DFD_WORKSPACE/data_dbw/crop_img"\
-    -t "../$DFD_WORKSPACE/data_dbw/trans_matrix"\
-    -r "1024"
-
-$DFD_PYTHON "preprocessor.py" \
-    -i "../$DFD_WORKSPACE/data_wyj/origin"\
-    -m "../$DFD_WORKSPACE/data_wyj/face_mask"\
-    -f "../$DFD_WORKSPACE/data_wyj/face_img"\
-    -c "../$DFD_WORKSPACE/data_wyj/crop_img"\
-    -t "../$DFD_WORKSPACE/data_wyj/trans_matrix"\
-    -r "1024"
+    -r "128"
